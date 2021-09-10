5f43be85bcb8680f9d09dc7770a971f1c6f66fbd
vvvuo <vvvuo@DESKTOP-4RV81TU>
2021-09-10 09:27:38 +0300

    Add project files.

---
+++ GitHarjoitus/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace GitHarjoitus
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
