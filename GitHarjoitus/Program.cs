c800f2df90507a94c081735f9e575904c20c052b
Valtteri Vuojärvi <valtteri20109@student.hamk.fi>
2021-09-10 09:37:33 +0300

    Lisäsin laskin luokan ja erotus metodin.

--- GitHarjoitus/Program.cs
+++ GitHarjoitus/Program.cs
@@ -7,6 +7,10 @@ namespace GitHarjoitus
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            Laskin laskimeni = new Laskin();
+            Console.WriteLine("Laske lukujen 148 ja 56 erotus");
+            int erotus = laskimeni.Erotus(148, 56);
+            Console.WriteLine("Erotus on: " + erotus);
         }
     }
 }
